bdb82e935d809272f47923d58dca820eca9fc662
kanhaiya kumar <kumarkhg4@gmail.com>
2021-01-31 06:28:52 +0530

    A little change

--- client/src/context/SocketContext.js
+++ client/src/context/SocketContext.js
@@ -2,7 +2,7 @@ import React, { useState, createContext } from "react";
 import io from "socket.io-client";
 export const SocketContext = createContext(null);
 //http://localhost:55000 https://complaintlodger.herokuapp.com
-const socket = io("https://complaintlodger.herokuapp.com",{
+const socket = io(process.env.REACT_APP_BACKEND_URL,{
     query:{
         token:localStorage.getItem('accesstoken')
     }
